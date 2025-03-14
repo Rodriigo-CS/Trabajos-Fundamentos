50cdfab08d7b2f752f02594a4db49e4fac8e2262
Rodriigo-CS <rodrigo2250152@correo.uis.edu.co>
2025-03-14 17:21:52 -0500

    .

--- Lista_de_N_números_divisibles_entre_2,3_o_5.py
+++
@@ -1,6 +0,0 @@
-Num = int(input("Ingrese el número maximo de la lista: "))
-for i in range(0,Num + 1):#El range va de 0 a Num + 1 para que el número ingresado sea incluido en la lista
-    if i % 2 == 0 or i % 3 == 0 or i % 5 == 0:#Se evalua si el número es divisible entre 2, 3 o 5 utilizando el or
-        print(i)
-    else:
-        pass#Si no es divisible entre 2, 3 o 5 el número no se imprime y continua el ciclo
