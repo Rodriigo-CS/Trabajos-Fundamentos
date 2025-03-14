6520ef0ab7ea2c2bd74235f97bfad91c85d943d4
Rodriigo-CS <rodrigo2250152@correo.uis.edu.co>
2025-03-14 17:50:05 -0500

    Taller 1 Ejercicio 3

---
+++ 3.RaizCuadradaEntera.py
@@ -0,0 +1,5 @@
+Num = int(input("Ingrese un número: "))
+if (Num ** 0.5) % 1 == 0:
+    print("El número", Num, "tiene raiz cuadrada entera.")
+else:
+    print("El número", Num, "no tiene raiz cuadrada entera.")
