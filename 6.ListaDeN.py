96e4512360490a9922a93c93ee7a7f19cabc106b
Rodriigo-CS <rodrigo2250152@correo.uis.edu.co>
2025-03-15 16:21:13 -0500

    Taller 1 ejercicio 6

---
+++ 6.ListaDeN.py
@@ -0,0 +1,19 @@
+Num = int(input("Ingrese un número: "))
+for i in range(1, Num + 1):
+    if i % 2 == 0:
+        Divisores = str("Es divisible por ") + str(2)
+    if i % 3 == 0:
+        Divisores = str("Es divisible por ") + str(3)
+    if i % 5 == 0:
+        Divisores = str("Es divisible por ") + str(5)
+    if i % 2 == 0 and i % 3 == 0:
+        Divisores = str("Es divisible por ") + str(2) + str(" y ") + str(3)
+    if i % 2 == 0 and i % 5 == 0:
+        Divisores = str("Es divisible por ") + str(2) + str(" y ") + str(5)
+    if i % 3 == 0 and i % 5 == 0:
+        Divisores = str("Es divisible por ") + str(3) + str(" y ") + str(5)
+    if i % 2 == 0 and i % 3 == 0 and i % 5 == 0:
+        Divisores = str("Es divisible por ") + str(2) + str(",") + str(3) + str(" y ") + str(5)
+    if i % 2 != 0 and i % 3 != 0 and i % 5 != 0:
+        Divisores = "No tiene divisores"
+    print(i, Divisores)
