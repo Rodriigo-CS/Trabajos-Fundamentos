838491abf09f4d6b3865d942c45ead7920152277
Rodriigo-CS <rodrigo2250152@correo.uis.edu.co>
2025-03-15 16:54:18 -0500

    Taller 1 Ejercicio 7

--- 7.ConteoPyN.py
+++ 7.ConteoPyN.py
@@ -1,1 +1,36 @@
 Num = int(input("Ingrese un número: "))
+Num0 = 0
+ConteoP = 0
+SumaP = 0
+ConteoN = 0
+SumaN = 0
+if Num < 0:
+    print("El número es negativo.")
+    ConteoN += 1
+    SumaN += Num
+elif Num == 0:
+    print("El número es cero.")
+    Num0 = 1
+else:
+    print("El número es positivo.")
+    ConteoP += 1
+    SumaP += Num
+while Num != 0:
+    X = int(input("Ingrese un número: "))
+    Num = X + Num
+    print("El Número Acumulado es: ", Num)
+    if X < 0:
+        ConteoN += 1
+        SumaN += X
+    elif X > 0:
+        ConteoP += 1
+        SumaP += X
+    else:
+        pass
+if Num0 == 1:
+    print("No se ingresaron números positivos ni negativos.")
+else:
+    print("La cantidad de números positivos son: ", ConteoP)
+    print("La cantidad de números negativos son: ", ConteoN)
+    print("El promedio de los números positivos es: ", SumaP/ConteoP)
+    print("El promedio de los números negativos es: ", SumaN/ConteoN)
